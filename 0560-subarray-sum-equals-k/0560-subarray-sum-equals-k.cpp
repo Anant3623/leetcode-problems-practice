99270084b5e013c7d50249e280d2b4c36c49403a
Anant Kumar <141392426+Anant3623@users.noreply.github.com>
2026-08-20 18:38:52 +0530

    Time: 57 ms (35.25%), Space: 50.9 MB (21.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -6,15 +6,15 @@ public:
         int result=0;
         unordered_map<int,int>mp;
         mp[0]=1;
-        int diff=0;
-        int f=0;
+       
+        
        
        
         
         for(int i=0;i<n;i++){
           sum+=v[i];
-          diff=sum-k;
-          result+=mp[diff];
+         
+          result+=mp[sum-k];
           mp[sum]++;
           
         }
