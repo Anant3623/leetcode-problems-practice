dd0d14f988da7305ef53ceba49eb380fce1be260
Anant Kumar <141392426+Anant3623@users.noreply.github.com>
2026-08-21 00:50:35 +0530

    Time: 20 ms (79.85%), Space: 88.3 MB (73.04%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -4,7 +4,7 @@ public:
         int sum=0;
         unordered_map<int,int>mp;
         int ans=0;
-
+        mp[0]=-1;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0){
                 sum+=-1;
@@ -16,6 +16,7 @@ public:
             if(sum==0){
                 ans=max(ans,i+1);
             }
+            
             if(mp.contains(sum)){
                 ans=max(ans,(i-mp[sum]));
             }
